6155fc42a8d3eb419f3b6ffbb69282bf0ff7708a
Gurkengewuerz <niklas@schuetrumpf.net>
2021-12-26 19:04:08 +0100

    fixed docker build

--- docker-build.sh
+++ docker-build.sh
@@ -4,9 +4,9 @@ PROJECT="serial-ssh"
 REGISTRY="docker.io"
 
 docker buildx create --use --name ${USERNAME}-${PROJECT}
-docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64 -t ${USERNAME}/${PROJECT}:latest .
-docker tag ${USERNAME}/${PROJECT}:latest ${REGISTRY}/${USERNAME}/${PROJECT}:latest
-docker push ${REGISTRY}/${USERNAME}/${PROJECT}
+docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64 --push -t ${REGISTRY}/${USERNAME}/${PROJECT}:latest .
+docker buildx stop ${USERNAME}-${PROJECT}
+docker buildx rm ${USERNAME}-${PROJECT}
 
 echo -e "Done!"
 
